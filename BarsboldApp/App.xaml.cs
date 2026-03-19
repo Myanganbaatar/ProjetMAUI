ef4671111f35ca03570df0bb1905854936099585
Barsbold MYANGANBAATAR <barsbold.myanganbaatar@etu.unilim.fr>
2026-03-19 15:27:51 +0100

    Ajout de la page accueil

---
+++ BarsboldApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BarsboldApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BarsboldApp/Views/AccueilPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace BarsboldApp.Views;
+
+public partial class AccueilPage : ContentPage
+{
+    public AccueilPage()
+    {
+        InitializeComponent();
+    }
+}
