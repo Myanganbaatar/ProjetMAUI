152e93fd1bcc2737c484b3003bc6bba890db2006
Barsbold MYANGANBAATAR <barsbold.myanganbaatar@etu.unilim.fr>
2026-03-24 16:55:52 +0100

    ajout du MainViewModel avec les propriétés MainTitle et SubTitle

---
+++ BarsboldApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BarsboldApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string mainTitle = "Explorateur de Pays";
+        private string subTitle = "Le monde à portée de main";
+
+        public string MainTitle
+        {
+            get => mainTitle;
+            set
+            {
+                if (mainTitle != value)
+                {
+                    mainTitle = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string SubTitle
+        {
+            get => subTitle;
+            set
+            {
+                if (subTitle != value)
+                {
+                    subTitle = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
