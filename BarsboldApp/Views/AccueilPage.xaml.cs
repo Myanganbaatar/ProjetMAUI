e55cea346855801c5c67ca25127c2d7538a43ca3
Barsbold MYANGANBAATAR <barsbold.myanganbaatar@etu.unilim.fr>
2026-03-24 16:56:13 +0100

    mise en place de l'entête de la page d'accueil

--- BarsboldApp/Views/AccueilPage.xaml.cs
+++ BarsboldApp/Views/AccueilPage.xaml.cs
@@ -1,3 +1,5 @@
+using BarsboldApp.ViewModels;
+
 namespace BarsboldApp.Views;
 
 public partial class AccueilPage : ContentPage
@@ -5,5 +7,6 @@ public partial class AccueilPage : ContentPage
     public AccueilPage()
     {
         InitializeComponent();
+        BindingContext = new MainViewModel();
     }
 }
