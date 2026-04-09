b8db2825d9d0a8bceb794a3df53c59dcc495d52c
João Santos <joao.santos@etu.unilim.fr>
2026-04-09 13:55:24 +0200

    feat(views): logique de liaison pour ParametresPage

--- BarsboldApp/Views/ParametresPage.xaml.cs
+++ BarsboldApp/Views/ParametresPage.xaml.cs
@@ -1,9 +1,12 @@
+using BarsboldApp.ViewModels;
+
 namespace BarsboldApp.Views;
 
 public partial class ParametresPage : ContentPage
 {
-    public ParametresPage()
-    {
-        InitializeComponent();
-    }
+	public ParametresPage()
+	{
+		InitializeComponent();
+		BindingContext = new ParametresViewModel();
+	}
 }
