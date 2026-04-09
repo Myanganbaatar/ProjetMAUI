1f19b68c5ad3f375b8bf7cb60726d98a46dbea17
João Santos <joao.santos@etu.unilim.fr>
2026-04-09 13:53:55 +0200

    feat(viewmodels): logique IsDarkMode avec sauvegarde des Preferences

--- BarsboldApp/ViewModels/ParametresViewModel.cs
+++ BarsboldApp/ViewModels/ParametresViewModel.cs
@@ -7,6 +7,32 @@ namespace BarsboldApp.ViewModels;
 
 public class ParametresViewModel : INotifyPropertyChanged
 {
+    private bool _isDarkMode;
+
+    public bool IsDarkMode
+    {
+        get => _isDarkMode;
+        set
+        {
+            if (_isDarkMode != value)
+            {
+                _isDarkMode = value;
+                OnPropertyChanged();
+                
+                
+                Application.Current.UserAppTheme = value ? AppTheme.Dark : AppTheme.Light;
+                
+              
+                Preferences.Default.Set("DarkModeChoisi", value);
+            }
+        }
+    }
+
+    public ParametresViewModel()
+    {
+        IsDarkMode = Preferences.Default.Get("DarkModeChoisi", false);
+    }
+    
     public event PropertyChangedEventHandler PropertyChanged;
     protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
     {
