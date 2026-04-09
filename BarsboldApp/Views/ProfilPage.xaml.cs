04f2e46790bcbdb0f63a7732b9cf3476b01b8b80
João Santos <joao.santos@etu.unilim.fr>
2026-04-09 13:55:54 +0200

    merge: integration de la logique fusionnee dans le Profil

--- BarsboldApp/Views/ProfilPage.xaml.cs
+++ BarsboldApp/Views/ProfilPage.xaml.cs
@@ -11,5 +11,32 @@ public partial class ProfilPage : ContentPage
     {
         InitializeComponent();
         _viewModel = viewModel;
+        
+        ItemsSection.BindingContext = new ProfilViewModel();
+        PaysSection.BindingContext = _viewModel;
+    }
+    
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        
+        
+        if (_viewModel.ListePays.Count == 0)
+        {
+            await _viewModel.ChargerPaysAsync();
+        }
+    }
+
+    
+    private async void SurPaysSelectionne(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.FirstOrDefault() is Country paysClique)
+        {
+            
+            ((CollectionView)sender).SelectedItem = null;
+
+           
+            await Navigation.PushAsync(new PaysDetailPage(paysClique));
+        }
     }
 }
