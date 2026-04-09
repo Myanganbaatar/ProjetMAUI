c6ebb408fff974ee3aadc9d330c26bf573827be7
João Santos <joao.santos@etu.unilim.fr>
2026-04-09 13:53:24 +0200

    ajout de la classe principale Country

--- BarsboldApp/Models/Country.cs
+++ BarsboldApp/Models/Country.cs
@@ -1,5 +1,17 @@
 namespace BarsboldApp.Models;
 
+public class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; } 
+    public string Capital { get; set; } 
+    public long Population { get; set; } 
+    
+
+    public CountryMedia Media { get; set; } 
+}
+
+
 public class CountryMedia
 {
     public string Flag { get; set; } 
